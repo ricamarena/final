158eb24d13fb47f98d060a0458ec64e0383fc9ea
Ricardo Camarena <rcamarena2016@kellogg.northwestern.edu>
2015-06-05 17:00:07 -0500

    Class 10

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  skip_before_action :auth, only: [:new, :create]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params["id"])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params["user"])
+    @user.admin == false
+    if @user.save
+      redirect_to projects_url
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params["id"])
+  end
+
+  def update
+   @user = User.find_by(id: params["id"])
+    if @user.update(params["user"])
+      redirect_to projects_url
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params["id"])
+    @user.destroy
+    redirect_to users_path
+  end
+
+end
